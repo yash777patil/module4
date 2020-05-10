9ab371e2c11b24663a849df9c3db199c78f5d388
yash777patil <63490653+yash777patil@users.noreply.github.com>
2020-05-10 18:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-
 (function (window) {
 
-var names = ["mark", "sam", "Jen", "Jason", "yash", "Frank", "jatin", "Paula", "jaya", "Jim"];
+  var names = ["mark", "sam", "Jen", "Jason", "yash", "Frank", "jatin", "Paula", "jaya", "Jim"];
 
-for (var i = 0; i < names.length; i++) {
-
- 
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
-
- 
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
+  for (var i = 0; i < names.length; i++) {
+     var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+     if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+     else {
+      helloSpeaker.speak(names[i]);
+     }
+   }
 
 })(window);
