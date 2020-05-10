0c9ba351ffef9d9512d6ca362e56cd60abaae95c
yash777patil <63490653+yash777patil@users.noreply.github.com>
2020-05-10 18:54:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+(function (window) {
+
+var names = ["mark", "sam", "Jen", "Jason", "yash", "Frank", "jatin", "Paula", "jaya", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+ 
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+ 
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})(window);
