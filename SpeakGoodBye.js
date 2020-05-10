a8b1d932797e986f2f6a4e59f7cb2c1699426b92
yash777patil <63490653+yash777patil@users.noreply.github.com>
2020-05-10 18:57:29 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,15 +1,14 @@
-
 (function (window) {
 
 
-var byeSpeaker = {};
+ var byeSpeaker = {};
 
 
-var speakWord = "Good Bye";
+ var speakWord = "Good Bye";
 
-byeSpeaker.speak =function(name) {
+ byeSpeaker.speak =function(name) {
 	console.log(speakWord+" " + name);
 }
-window.byeSpeaker =byeSpeaker;
+ window.byeSpeaker =byeSpeaker;
 
 })(window);
