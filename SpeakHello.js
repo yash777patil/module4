0868907399af83f8daa39ee990282bab8e5e3a9e
yash777patil <63490653+yash777patil@users.noreply.github.com>
2020-05-10 18:58:12 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,14 +1,13 @@
-
 (function(window) {
 
-var helloSpeaker = {};
+ var helloSpeaker = {};
 
-var speakWord = "Hello";
+ var speakWord = "Hello";
 
-helloSpeaker.speak= function(name) {
+ helloSpeaker.speak= function(name) {
 	console.log(speakWord + " " + name);
-}
+ }
 
-window.helloSpeaker=helloSpeaker;
+ window.helloSpeaker=helloSpeaker;
 
 })(window);
