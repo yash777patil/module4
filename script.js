5ccb85565897baad1fb421588f30e7fdac7b7743
yash777patil <63490653+yash777patil@users.noreply.github.com>
2020-05-10 19:15:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function (window) {
 
-  var names = ["mark", "sam", "Jen", "Jason", "yash", "Frank", "jatin", "Paula", "jaya", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i = 0; i < names.length; i++) {
      var firstLetter = ((names[i]).charAt(0)).toLowerCase();
